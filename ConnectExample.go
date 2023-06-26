9eccb5b2a9c522960ec9c3513d4d2addb01a4643
RemKeeper <78486275+RemKeeper@users.noreply.github.com>
2023-06-27 04:21:05 +0800

    Add files via upload

---
+++ ConnectExample.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
+func main() {
+	header := http.Header{}
+	header.Add("Key", "www.51xtzy.cn")
+	// 建立WebSocket连接
+	conn, _, err := websocket.DefaultDialer.Dial("ws://13.250.39.11:8080/ws", header)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	// 循环接收服务器发送的消息
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Printf("Received message: %s\n", message)
+	}
+}
